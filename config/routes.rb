8945d8ea2449fa97d52838cd863b9e72b52983dd
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2022-02-05 10:32:38 +0100

    routes added to rearange de version of the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories do
-    resources :products 
+  
+  namespace :api do
+    namespace :v1 do
+      resources :categories do
+        resources :products 
+      end
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
